70964cb5349dc256e9ed91139bde5ef957b22b0f
Ibrahim Gunawan <baimgunawan1@gmail.com>
2024-12-02 20:50:11 +0700

    Initialize GIT

---
+++ src/App.jsx
@@ -0,0 +1,69 @@
+/* eslint-disable react/prop-types */
+import { useState } from "react";
+
+function Square({ value, onSquareClick }) {
+  return (
+    <button className="square" onClick={onSquareClick}>
+      {value}
+    </button>
+  );
+}
+
+function validateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+
+  return false;
+}
+
+function Board() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  const [xIsNext, setXIsNext] = useState(true);
+  function handleClick(i) {
+    if (squares[i] || validateWinner(squares)) {
+      return alert("Kotak sudah terisi!");
+    }
+
+    const nextSquares = squares.slice();
+    nextSquares[i] = xIsNext ? "X" : "O";
+    setSquares(nextSquares);
+    setXIsNext(!xIsNext);
+  }
+  const winner = validateWinner(squares);
+  let status = winner ? `Winner ${winner}` : `Turn : ${xIsNext ? "X" : "O"}`;
+  console.log(status);
+
+  return (
+    <div className="container">
+      <h1>Tic-Tac-Toe Game</h1>
+      <div className="status">{status}</div>
+      <div className="board">
+        {squares.map((square, index) => (
+          <Square
+            key={index}
+            value={squares[index]}
+            onSquareClick={() => handleClick(index)}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Board;
